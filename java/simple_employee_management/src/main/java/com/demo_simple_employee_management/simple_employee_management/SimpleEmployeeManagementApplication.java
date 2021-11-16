a3bede0bb3082c89f832495a9bb6ac932e2f31f2
cristianofmc <cristiano.fmc@hotmail.com>
2021-11-16 19:17:38 -0300

    first commit, java spring initialized

---
+++ java/simple_employee_management/src/main/java/com/demo_simple_employee_management/simple_employee_management/SimpleEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.demo_simple_employee_management.simple_employee_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEmployeeManagementApplication.class, args);
+	}
+
+}
